fbf0b2a6d2c688ac735b8517975ccb54a0de5f9f
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2022-04-02 12:16:27 +0200

    Aggiunto Es6+correzioni su es5

--- lab1_es6/lab1_es6.ino
+++ lab1_es6/lab1_es6.ino
@@ -1,26 +1,35 @@
+#include <LiquidCrystal_PCF8574.h>
+LiquidCrystal_PCF8574 lcd(0x27);
+
 const int TEMP_PIN = A1;
 const int B = 4275;
 const int long R0 = 100000;
 const int Vcc = 1023;
 const float T0 = 298.15;
+
 void setup() {
   // put your setup code here, to run once:
   pinMode(TEMP_PIN, INPUT);
+
   Serial.begin(9600);
   while (!Serial);
-  Serial.println("Lab 1.5 Starting");
+  Serial.println("Lab 1.6 Starting");
+
+  lcd.begin(16, 2);
+  lcd.setBacklight(255);
+  lcd.home();
 }
 
-void convertTension (float sensorVal) {
+void convertTension (int sensorVal) {
   float R = ((Vcc / sensorVal) - 1.0) * R0;
   float T = 1 / (log(R / R0) / B + 1 / T0) - 273.15;
-  Serial.print("Temperatura: ");
-  Serial.println(T);
+  lcd.clear();
+  lcd.print("Temperature:");
+  lcd.print(T);
   delay(1e4);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  float sensorVal = analogRead(TEMP_PIN);
-  convertTension (sensorVal);
+  int sensorVal = analogRead(TEMP_PIN);
+  convertTension(sensorVal);
 }

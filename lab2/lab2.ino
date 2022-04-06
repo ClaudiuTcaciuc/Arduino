d362911b44ec3a9049ba40eef31ddf65c299eac8
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2022-04-06 23:25:38 +0200

    Ottimizzazioni by AR

--- lab2/lab2.ino
+++ lab2/lab2.ino
@@ -2,79 +2,72 @@
 const int FAN_PIN = 6;
 //led
 const int RLED_PIN = 11;
-float currentSpeed = 0;
 //sensore di temperatura
 const int TEMP_PIN = A1;
-const int B = 4275;
-const int long R0 = 100000;
-const int Vcc = 1023;
-const float T0 = 298.15;
 
 void setup() {
   pinMode(TEMP_PIN, INPUT);
   pinMode(RLED_PIN, OUTPUT);
   pinMode(FAN_PIN, OUTPUT);
-  analogWrite(FAN_PIN, (int) currentSpeed);
+  analogWrite(FAN_PIN, 0);
   Serial.begin(9600);
   while (!Serial);
   Serial.println("Lab 2 Starting");
 }
 
-float convertTension (float sensorVal) {
+float convertTension(float sensorVal) {
+  const int B = 4275;
+  const int long R0 = 100000;
+  const int Vcc = 1023;
+  const float T0 = 298.15;
+  const float TBase = 273.15;
   float R = ((Vcc / sensorVal) - 1.0) * R0;
-  float T = 1 / (log(R / R0) / B + 1 / T0) - 273.15;
+  float T = 1 / (log(R / R0) / B + 1 / T0) - TBase;
   Serial.print("Temperatura: ");
   Serial.println(T);
   return T;
 }
 
-int SetSpeed (float Temp) {
-  if (Temp >= 25) {
-    if (Temp <= 38) {
-      float DT = (Temp - 25) * 20; return (int)DT;
-    } else {
-      return 255;
-    }
+int calcSpeed(float temp) {
+  if (temp <= 38.0) {
+    float dt = (temp - 25.0) * 20.0;
+    return (int)dt;
   } else {
-    return 0;
+    return 255;
   }
 }
 
-int SetLed(float Temp) {
-  if (Temp < 25) {
-    if (Temp >= 20) {
-      float DT = (Temp - 20) * 51; return (int)DT;
-    } else {
-      return 255;
-    }
+int calcLedIntensity(float temp) {
+  if (temp >= 20.0) {
+    float dt = (temp - 20.0) * 51.0;
+    return static_cast<int>(dt);
   } else {
-    return 0;
+    return 255;
   }
 }
 
-void change(int Temp) {
-  if (Temp >= 25) {
-    analogWrite(RLED_PIN, LOW)
-    int newSpeed = SetSpeed(Temp);
-    changeSpeed(newSpeed);
+void changeFanSpeed(int newSpeed) {
+  if (newSpeed <= 255 && newSpeed >= 0) {
+    analogWrite(FAN_PIN, newSpeed);
+  }
+}
+
+void checkTempAndChangeSpeed(float temp) {
+  if (temp >= 25.0) {
+    analogWrite(RLED_PIN, LOW);
+    int newSpeed = calcSpeed(temp);
+    changeFanSpeed(newSpeed);
   }
   else {
-    changeSpeed(0);
-    int newLed = SetLed(Temp);
+    changeFanSpeed(0);
+    int newLed = calcLedIntensity(temp);
     analogWrite(RLED_PIN, newLed);
-  }
-}
-
-void changeSpeed(int new_speed) {
-  if (new_speed <= 255 && new_speed >= 0) {
-    currentSpeed = new_speed;
-    analogWrite(FAN_PIN, (int) currentSpeed);
   }
 }
 
 void loop() {
   float sensorVal = analogRead(TEMP_PIN);
-  float Temp = convertTension (sensorVal);
-  change(Temp);
+  float temp = convertTension(sensorVal);
+  checkTempAndChangeSpeed(temp);
   delay(1e4);
 }

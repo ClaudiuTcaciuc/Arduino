8aec1b586fc8e8db086d3e79038aca69e3e06caa
ClaudiuTcaciuc <102698780+ClaudiuTcaciuc@users.noreply.github.com>
2022-04-14 02:02:22 +0200

    ALmost done :)

--- lab1_parte2_completo/lab1_parte2_completo.ino
+++ lab1_parte2_completo/lab1_parte2_completo.ino
@@ -15,8 +15,10 @@ const int TEMP_PIN = A1;
 const int PIR_PIN = 7;
 unsigned long elapsed_time = 0;
 const long timeout_pir = 30l * 60l * 1000l;
-const int display_period = 5000;
-unsigned long previous_time = 0;
+const int display1_period = 5000;
+const int display2_period = 10000;
+unsigned long previous_time_display1 = 0;
+unsigned long previous_time_display2 = 0;
 int flag_changed = 0;
 struct TempPair {
   float low;
@@ -74,13 +76,13 @@ int calcLedIntensity(float temp) {
   }
 }
 
-void checkTempAndChangeSpeed(float temp, int pres) {
+void checkTempAndChangeSpeed(float temp, int pres, unsigned long time_point) {
   int newSpeed = 0;
   int newLedIntensity = 0;
   if (pres == 0)
     setNoOneSetPoint();
-  else{
-    if(flag_changed == 0)
+  else {
+    if (flag_changed == 0)
       setDefaultSetPoint();
   }
   if (temp >= ct.low) {
@@ -88,8 +90,6 @@ void checkTempAndChangeSpeed(float temp, int pres) {
     newSpeed = calcSpeed(temp);
     if (newSpeed <= 255 && newSpeed >= 0) {
       analogWrite(FAN_PIN, newSpeed);
-      Serial.print("New fan speed: ");
-      Serial.println(newSpeed);
     }
   }
   else if (temp < ht.high) {
@@ -97,8 +97,6 @@ void checkTempAndChangeSpeed(float temp, int pres) {
     newLedIntensity = calcLedIntensity(temp);
     if (newLedIntensity <= 255 && newLedIntensity >= 0) {
       analogWrite(RLED_PIN, newLedIntensity);
-      Serial.print("New led intensity: ");
-      Serial.println(newLedIntensity);
     }
   }
   else {
@@ -106,8 +104,7 @@ void checkTempAndChangeSpeed(float temp, int pres) {
     analogWrite(FAN_PIN, 0);
   }
   String bufPage1, bufPage2;
-  unsigned long current_time = millis();
-  if (current_time - previous_time >= display_period) {
+  if (time_point >= previous_time_display1 + display1_period) {
     lcd.clear();
     lcd.setCursor(0, 0);
     bufPage1 = String("AC m:") + String(ct.low, 1) + " M:" + String(ct.high, 1);
@@ -115,8 +112,9 @@ void checkTempAndChangeSpeed(float temp, int pres) {
     lcd.print(bufPage1);
     lcd.setCursor(0, 1);
     lcd.print(bufPage2);
-    previous_time = current_time;
-  } else {
+    previous_time_display1 += display1_period;
+  }
+  if (time_point >= previous_time_display2 + display2_period) {
     lcd.clear();
     lcd.setCursor(0, 0);
     bufPage1 = String("T:") + String(temp) + " Pres:" + pres;
@@ -124,36 +122,36 @@ void checkTempAndChangeSpeed(float temp, int pres) {
     lcd.print(bufPage1);
     lcd.setCursor(0, 1);
     lcd.print(bufPage2);
+    previous_time_display2 += display2_period;
   }
+
 }
 
 void checkPresencePIR() {
   if ( digitalRead(PIR_PIN) == HIGH ) {
     elapsed_time = millis();
-    Serial.println("PIR detected a movement");
+    //Serial.println("PIR detected a movement");
   }
 }
 
-void checkPresenceMIC() {
+void checkPresenceMIC(unsigned long time_point) {
   if (digitalRead(MIC_PIN) == LOW) {
-    if (millis() - last_event_time > 1000) {
+    if (time_point - last_event_time > 1000) {
       n_sound_events++;
-      last_event_time = millis();
-      Serial.println("MIC detected sound");
-      Serial.print("Number of events detected: ");
-      Serial.println(n_sound_events);
+      last_event_time = time_point;
+      //Serial.println("MIC detected sound");
     }
   }
 }
-bool checkPresenceRoom() {
+bool checkPresenceRoom(unsigned long time_point) {
   bool check_mic = true, check_pir = true;
-  checkPresenceMIC();
-  if (n_sound_events <= 50 && last_event_time + sound_interval <= millis()) {
+  checkPresenceMIC(time_point);
+  if (n_sound_events <= 50 && last_event_time + sound_interval <= time_point) {
     Serial.println("No detection for over 10 mins with sound sensor");
     n_sound_events = 0;
     check_mic = false;
   }
-  if (elapsed_time + timeout_pir <= millis()) {
+  if (elapsed_time + timeout_pir <= time_point) {
     Serial.println("No detection for over 30 mins with pir sensor");
     check_pir = false;
   }
@@ -178,12 +176,11 @@ void setNoOneSetPoint() {
 }
 
 void updateFormatError() {
-  Serial.println("Error in command format");
+  Serial.println("Error in command format or value not acceptable");
   return;
 }
 
 void checkCharAndUpdateVal(char c, TempPair & temp, float value) {
-  Serial.println(value);
   switch (c) {
     case 'L':
       temp.low = value;
@@ -202,6 +199,7 @@ void checkCharAndUpdateVal(char c, TempPair & temp, float value) {
   "CTLxxx" => ct.low
   "CTHxxx" => ct.high
 */
+
 void checkAndChangeSetPoint() {
   if (Serial.available() > 0) {
     auto command = Serial.readString();
@@ -223,23 +221,22 @@ void checkAndChangeSetPoint() {
   }
 }
 
-void setTempBasedOnPresence(float temp) {
+void setTempBasedOnPresence(float temp, unsigned long time_point) {
   int pres = 0;
-  if (checkPresenceRoom() == true) {
+  if (checkPresenceRoom(time_point) == true) {
     //Serial.println("There is at least one person is this room");
     pres = 1;
   }
   else
     pres = 0;
-  checkTempAndChangeSpeed(temp, pres);
+  checkTempAndChangeSpeed(temp, pres, time_point);
 }
 
 void loop() {
+  unsigned long time_point = millis();
+  checkAndChangeSetPoint();
   float sensorVal = analogRead(TEMP_PIN);
   float temp = convertTension(sensorVal);
-  Serial.print("Temperatura: ");
-  Serial.println(temp);
-  checkAndChangeSetPoint();
-  setTempBasedOnPresence(temp);
+  setTempBasedOnPresence(temp, time_point);
   delay(1e3);
 }
